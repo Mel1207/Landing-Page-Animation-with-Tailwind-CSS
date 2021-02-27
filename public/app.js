091fbca89b252f6fb71af6459df33821186cdd2e
Mel1207 <meljohnpualon@gmail.com>
2021-02-28 06:06:50 +0800

    added timeline functionality

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
 // console.log('hello world');
 
-gsap.from('.greetings', {opacity: 0, duration: 3})
+// gsap.from('.greetings', {opacity: 0, duration: 3})
+
+let tl = gsap.timeline({defaults: {duration: 2.5}})
+
+    tl.from(".greetings", {opacity: 0})
+    tl.to(".greetings", {display: "none"}, "-=2")
