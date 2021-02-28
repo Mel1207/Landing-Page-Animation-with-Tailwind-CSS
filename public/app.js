76b5080a5183ec017c09abb1c30d6441f2de0d2f
Mel1207 <meljohnpualon@gmail.com>
2021-03-01 05:48:56 +0800

    added animation for h2 element

--- public/app.js
+++ public/app.js
@@ -2,8 +2,8 @@
 
 // gsap.from('.greetings', {opacity: 0, duration: 3})
 
-let tl = gsap.timeline({defaults: {duration: 2.5}})
+let tl = gsap.timeline({defaults: {duration: 2}})
 
     tl.from(".greetings", {opacity: 0})
-    tl.to(".greetings", {display: "none"}, "-=2")
-    // tl.from(".sequences")
+    tl.to(".greetings", {opacity: 0})
+    tl.from("h2", {opacity: 0})
